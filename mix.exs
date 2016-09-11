6cd9ed270bbee1c0f001c19e47c30daec34ea286
Demarius Chrite <chrite.demarius@gmail.com>
2016-09-11 13:00:52 -0400

    Kill `shards`.

--- mix.exs
+++ mix.exs
@@ -38,8 +38,7 @@ defmodule Dasher.Mixfile do
       {:phoenix_html, "~> 2.6"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:shards, "0.3.1"}
+      {:cowboy, "~> 1.0"}
     ]
   end
 
